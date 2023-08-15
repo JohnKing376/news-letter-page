572f8b08c42d2105c4e626dd0b245847f1c2c8ae
JohnKing <110711668+JohnKing376@users.noreply.github.com>
2023-08-15 06:14:32 -0500

    Adding Support for Mobile

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+// Form Settings
+
 const  form = document.getElementById('form');
 const email = document.getElementById('contactEmail');
 const errorMessage = document.querySelector('.form-error-msg');
-const input = document.querySelector('input')
+const input = document.querySelector('input');
+const emailInput = document.querySelector('.emailInput');
+const popUp = document.getElementById('successPopup');
+const containerHidden = document.querySelector('.container')
 
-form.addEventListener('submit', ()=>{
-checkInputs()
+form.addEventListener('submit', (e)=>{
+checkInputs();
+e.preventDefault();
 })
 
 
@@ -15,13 +21,16 @@ function checkInputs() {
         errorMessage.innerHTML = "Email is required"
         errorMessage.classList.add('error-active');
         input.classList.add('input-error-msg')
+        closePopup();
     } 
     else if(!isEmail(email.value)) {
         errorMessage.innerHTML = "Enter Valid Email"
         errorMessage.classList.add('error-active');
+        closePopup();
     } else {
         errorMessage.classList.remove('error-active');
-        input.classList.remove('input-error-msg')
+        input.classList.remove('input-error-msg');
+        emailInput.innerHTML = email.value
     }
 
 }
@@ -31,3 +40,14 @@ function isEmail(email) {
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
 }
+
+
+function openPopup() {
+    popUp.classList.add('openPopup');
+    containerHidden.classList.add('container-remove');
+}
+function closePopup() {
+    popUp.classList.remove('openPopup')
+    containerHidden.classList.remove('container-remove');
+}
+
